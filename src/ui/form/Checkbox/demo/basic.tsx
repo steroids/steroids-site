ff336f56624e20730647025fb6ab1d1fe35d0b5b
Dmitriy <kraxtv471365@gmail.com>
2023-02-21 14:17:24 +0700

    Added checkbox demos

---
+++ src/ui/form/Checkbox/demo/basic.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { CheckboxField} from '@steroidsjs/core/ui/form';
+
+/**
+ * Обычный пример использования CheckboxField.
+ * @order 1
+ * @col 4
+ */
+
+export default () => (
+    <>
+        <CheckboxField label='Remember me' />
+    </>
+);
