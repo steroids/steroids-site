a3996f44dcaaa209b23d0aff553ddb4e3dc86e3e
Dmitriy <kraxtv471365@gmail.com>
2023-03-13 11:43:37 +0700

    Added PasswordField demos

---
+++ src/ui/form/PasswordField/demo/disabled.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {PasswordField} from '@steroidsjs/core/ui/form';
+
+/**
+ * Выключенный или недоступный для использования.
+ * @order 2
+ * @col 3
+ */
+
+export default () => (
+    <>
+        <PasswordField label='Disabled' disabled />
+    </>
+);
---
+++ src/ui/form/PasswordField/demo/placeholder.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {PasswordField} from '@steroidsjs/core/ui/form';
+
+/**
+ * Сообщение пользователю для заполнения по контексту использования.
+ * @order 4
+ * @col 3
+ */
+
+export default () => (
+    <>
+        <PasswordField label='Placeholder' placeholder='Your password...' />
+    </>
+);
