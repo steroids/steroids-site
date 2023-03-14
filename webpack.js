2d958e6c45a8e9100c6642a2b51151ff5d98a233
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-03-14 15:59:09 +0800

    Webpack for dev

--- webpack.js
+++ webpack.js
@@ -1,4 +1,21 @@
+// require('@steroidsjs/webpack').config({
+//     inlineSvg: true,
+//     port: 9991,
+// });
+
+const path = require('path');
+
+const STEROIDS_PATH = '../../dev';
+
 require('@steroidsjs/webpack').config({
-    inlineSvg: true,
     port: 9991,
+    inlineSvg: true,
+    webpack: {
+        resolve: {
+            alias: {
+                '@steroidsjs/core': path.resolve(__dirname, STEROIDS_PATH + '/steroids/react/src'),
+                '@steroidsjs/bootstrap': path.resolve(__dirname, STEROIDS_PATH + '/steroids/react-bootstrap/src'),
+            },
+        },
+    },
 });
