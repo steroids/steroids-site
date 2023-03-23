8b9ff632e4d47af61b2181bd7869895e5050954b
Dmitriy Babiy <dimascelecto@gmail.com>
2023-03-23 17:00:33 +0700

    Add demo Tooltip

--- webpack.js
+++ webpack.js
@@ -1,4 +1,17 @@
+const path = require('path');
+
+const STEROIDS_PATH = '../../dev';
+
 require('@steroidsjs/webpack').config({
     inlineSvg: true,
     port: 9991,
+    inlineSvg: true,
+    webpack: {
+        resolve: {
+            alias: {
+                '@steroidsjs/core': path.resolve(__dirname, '../react/src'),
+                '@steroidsjs/bootstrap': path.resolve(__dirname, '../react-bootstrap/src'),
+            },
+        },
+    },
 });
