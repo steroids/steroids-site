2f861f42eb2525d2a09a2bd699a692fbdc491ed5
Dmitriy <kraxtv471365@gmail.com>
2023-03-27 13:18:45 +0700

    Demos for Card component

--- src/ui/content/Card/demo/All-variants.tsx
+++ src/ui/content/Card/demo/All-variants.tsx
@@ -2,17 +2,138 @@
 import * as React from 'react';
 import Card from '@steroidsjs/core/ui/content/Card';
 import {ICardProps} from '@steroidsjs/core/ui/content/Card/Card';
+import {IButtonProps} from '@steroidsjs/core/ui/form/Button/Button';
 
 const CARD_TITLE = 'Card Title';
 const CARD_DESCRIPTION = 'Some quick example text to build on the card title and make up the bulk of the cards content.';
 
+const buttons = [
+    {
+        outline: true,
+        color: 'primary',
+        children: 'PRIMARY',
+        size: 'sm',
+    },
+    {
+        outline: false,
+        color: 'primary',
+        children: 'PRIMARY',
+        size: 'sm',
+
+    },
+] as IButtonProps[];
+
+const cover = 'https://i.ibb.co/1rTqmJD/image-1.jpg';
+
+const links = [
+    {
+        url: 'https://i.ibb.co/',
+        text: 'Link',
+    },
+    {
+        url: 'https://i.ibb.co/',
+        text: 'Another link',
+    },
+];
+
+export const header = {
+    avatar: {
+        src: 'https://i.ibb.co/T4j2NMQ/20230325210515-1.jpg',
+        status: true,
+    },
+    head: 'Header',
+    subhead: 'Subhead',
+    menu: true,
+};
+
+const footer = {
+    head: 'Footer',
+    subhead: 'Subhead',
+};
+
 const variants: Record<string, ICardProps[]> = {
-    row: [
+    row1: [
         {
-            cover: 'https://i.ibb.co/1rTqmJD/image-1.jpg',
+            header,
+            cover,
             title: CARD_TITLE,
             description: CARD_DESCRIPTION,
-            footer: 'Footer',
+            buttons,
+        },
+        {
+            header,
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+        },
+        {
+            header,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            links,
+        },
+    ],
+    row2: [
+        {
+            header,
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            buttons,
+            footer,
+        },
+        {
+            header,
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            footer,
+        },
+        {
+            header,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            links,
+            footer,
+        },
+    ],
+    row3: [
+        {
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            buttons,
+        },
+        {
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+        },
+        {
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            links,
+        },
+    ],
+    row4: [
+        {
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            footer,
+            buttons,
+        },
+        {
+            cover,
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            footer,
+        },
+        {
+            title: CARD_TITLE,
+            description: CARD_DESCRIPTION,
+            links,
+            footer,
         },
     ],
 };
@@ -24,12 +145,19 @@ const variants: Record<string, ICardProps[]> = {
  */
 
 export default () => (
-    <div>
-        {Object.entries(variants).map(([row, cards]) => cards.map((card, cardIndex) => (
-            <Card
-                {...card}
-                key={cardIndex}
-            />
-        )))}
+    <div style={{display: 'flex', flexFlow: 'column wrap', rowGap: '30px'}}>
+        {Object.entries(variants).map(([_, cards], rowIndex) => (
+            <div
+                key={rowIndex}
+                style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', columnGap: '20px'}}
+            >
+                {cards.map((card, cardIndex) => (
+                    <Card
+                        {...card}
+                        key={cardIndex}
+                    />
+                ))}
+            </div>
+        ))}
     </div>
 );
---
+++ src/ui/content/Card/demo/header.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable max-len */
+import * as React from 'react';
+import Card from '@steroidsjs/core/ui/content/Card';
+import {ICardProps} from '@steroidsjs/core/ui/content/Card/Card';
+import {IButtonProps} from '@steroidsjs/core/ui/form/Button/Button';
+import {header} from './All-variants';
+
+const cards = [
+    {
+        header,
+    },
+    {
+        header: {
+            ...header,
+            avatar: undefined,
+        },
+    },
+] as ICardProps[];
+
+/**
+ * Body
+ * @order 4
+ * @col 12
+ */
+
+export default () => (
+    <div style={{display: 'flex', flexFlow: 'column wrap', rowGap: '20px'}}>
+        {cards.map((card, cardIndex) => (
+            <Card
+                key={cardIndex}
+                {...card}
+            />
+        ))}
+    </div>
+);
