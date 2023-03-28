9992de1ddbdbe14ba3e073a1f7077fe0e7aa242c
Dmitriy Babiy <dimascelecto@gmail.com>
2023-03-28 15:34:26 +0700

    Tooltip demo fix

--- src/ui/layout/Tooltip/demo/basic.tsx
+++ src/ui/layout/Tooltip/demo/basic.tsx
@@ -7,123 +7,89 @@ import Tooltip from '@steroidsjs/core/ui/layout/Tooltip';
  * @order 1
  * @col 6
  */
-const examples = {
-    row1: [
-        'topLeft',
-        'top',
-        'topRight',
-    ],
-    row2: [
-        'bottomLeft',
-        'bottom',
-        'bottomRight',
-    ],
-    column1: [
-        'leftTop',
-        'left',
-        'leftBottom',
-    ],
-    column2: [
-        'rightTop',
-        'right',
-        'rightBottom',
-    ],
+
+const examplesRows = {
+    row1: {
+        topLeft: 'topLeft',
+        top: 'top',
+        topRight: 'topRight',
+    },
+    row2: {
+        bottomLeft: 'bottomLeft',
+        bottom: 'bottom',
+        bottomRight: 'bottomRight',
+    },
+};
+
+const examplesColumns = {
+    column1: {
+        leftTop: 'leftTop',
+        left: 'left',
+        leftBottom: 'leftBottom',
+    },
+    column2: {
+        rightTop: 'rightTop',
+        right: 'right',
+        rightBottom: 'rightBottom',
+    },
 };
 
 export default () => (
     <>
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                gridGap: '30px',
-                padding: '30px',
-            }}
-        >
-            {examples.row1.map((position, index) => (
-                <Tooltip
-                    key={index}
-                    defaultVisible
-                    position={position}
-                    content={position}
-                >
-                    <div style={{width: 'fit-content'}}>
-                        <Button label='Show Tooltip' />
-                    </div>
-                </Tooltip>
-            ))}
-        </div>
-
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                gridGap: '30px',
-                padding: '30px',
-            }}
-        >
-            {examples.row2.map((position, index) => (
-                <Tooltip
-                    key={index}
-                    defaultVisible
-                    position={position}
-                    content={position}
-                >
-                    <div style={{width: 'fit-content'}}>
-                        <Button label='Show Tooltip' />
-                    </div>
-                </Tooltip>
-            ))}
-        </div>
-
+        {Object.entries(examplesRows).map(([examplesRow, positions]) => (
+            <div
+                key={examplesRow}
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gridGap: '30px',
+                    padding: '30px',
+                }}
+            >
+                {Object.entries(positions).map(([position]) => (
+                    <Tooltip
+                        key={position}
+                        defaultVisible
+                        position={position}
+                        content={position}
+                    >
+                        <div style={{width: 'fit-content'}}>
+                            <Button label='Show Tooltip' />
+                        </div>
+                    </Tooltip>
+                ))}
+            </div>
+        ))}
         <div style={{
             display: 'flex',
             justifyContent: 'center',
             paddingTop: '30px',
         }}
         >
-            <div
-                style={{
-                    display: 'flex',
-                    flexFlow: 'column',
-                    gridGap: '50px',
-                    padding: '30px',
-                }}
-            >
-                {examples.column1.map((position, index) => (
-                    <Tooltip
-                        key={index}
-                        defaultVisible
-                        position={position}
-                        content={position}
-                    >
-                        <div style={{width: 'fit-content'}}>
-                            <Button label='Show Tooltip' />
-                        </div>
-                    </Tooltip>
-                ))}
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    flexFlow: 'column',
-                    gridGap: '50px',
-                    padding: '30px',
-                }}
-            >
-                {examples.column2.map((position, index) => (
-                    <Tooltip
-                        key={index}
-                        defaultVisible
-                        position={position}
-                        content={position}
-                    >
-                        <div style={{width: 'fit-content'}}>
-                            <Button label='Show Tooltip' />
-                        </div>
-                    </Tooltip>
-                ))}
-            </div>
+            {Object.entries(examplesColumns).map(([examplesColumn, positions]) => (
+                <div
+                    key={examplesColumn}
+                    style={{
+                        display: 'flex',
+                        flexFlow: 'column',
+                        gridGap: '50px',
+                        padding: '30px',
+                    }}
+                >
+                    {Object.entries(positions).map(([position]) => (
+                        <Tooltip
+                            key={position}
+                            defaultVisible
+                            position={position}
+                            content={position}
+                        >
+                            <div style={{width: 'fit-content'}}>
+                                <Button label='Show Tooltip' />
+                            </div>
+                        </Tooltip>
+                    ))}
+                </div>
+            ))}
         </div>
     </>
 );
