df67631b2824cf7f7688a739c3805d8fa8ae761f
Dmitriy Babiy <dimascelecto@gmail.com>
2023-03-30 17:08:13 +0700

    Fix demo Tooltip

---
+++ src/ui/layout/Tooltip/demo/bottom-and-top.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {Button} from '@steroidsjs/core/ui/form';
+import Tooltip from '@steroidsjs/core/ui/layout/Tooltip';
+
+/**
+ * @order 1
+ * @col 6
+ */
+
+const examplesRows = {
+    row1: [
+        'topLeft',
+        'top',
+        'topRight',
+    ],
+    row2: [
+        'bottomLeft',
+        'bottom',
+        'bottomRight',
+    ],
+};
+
+export default () => (
+    <>
+        {Object.entries(examplesRows).map(([examplesRow, positions]) => (
+            <div
+                key={examplesRow}
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gridGap: '30px',
+                    padding: '10px 30px 30px 30px',
+                }}
+            >
+                {positions.map((position) => (
+                    <Tooltip
+                        key={position}
+                        defaultVisible
+                        position={position}
+                        content={position}
+                    >
+                        <div style={{width: 'fit-content'}}>
+                            <Button label='Show Tooltip' />
+                        </div>
+                    </Tooltip>
+                ))}
+            </div>
+        ))}
+    </>
+);
