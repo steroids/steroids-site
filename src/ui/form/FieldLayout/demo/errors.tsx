eab772a9533e8f2be7e343c138fa25e06ea6563d
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-05 13:24:01 +0700

    Add demo FieldLayout

---
+++ src/ui/form/FieldLayout/demo/errors.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {FieldLayout, InputField} from '@steroidsjs/core/ui/form';
+
+/**
+ * Обработка ошибок.
+ * @order 3
+ * @col 6
+ */
+
+export default () => (
+    <FieldLayout
+        label='Error'
+        errors={['The field is filled incorrectly']}
+    >
+        <InputField />
+    </FieldLayout>
+);
---
+++ src/ui/form/FieldLayout/demo/required.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {FieldLayout, InputField} from '@steroidsjs/core/ui/form';
+
+/**
+ * Как обязательное поле для заполнения.
+ * @order 2
+ * @col 6
+ */
+
+export default () => (
+    <FieldLayout
+        label='Required'
+        required
+    >
+        <InputField />
+    </FieldLayout>
+);
