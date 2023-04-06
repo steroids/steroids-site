81ac19b33c219f5bcd09d9a8dec3d387bad260c7
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-06 15:56:37 +0700

    Add demo for formatter

---
+++ src/ui/format/formatters/demo/boolean-formatter.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import BooleanFormatter from '@steroidsjs/core/ui/format/BooleanFormatter';
+
+export default () => (
+    <>
+        <BooleanFormatter
+            value='2018-05-04'
+        />
+    </>
+);
---
+++ src/ui/format/formatters/demo/date-time-formatter.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import DateTimeFormatter from '@steroidsjs/core/ui/format/DateTimeFormatter';
+
+export default () => (
+    <>
+        <DateTimeFormatter
+            value='2018-05-04 16:15:00'
+        />
+    </>
+);
