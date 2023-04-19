7d425e33ab9899940c5f4be1a470cf31f96e02e0
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-19 16:50:22 +0700

    Fix webpack

--- webpack.js
+++ webpack.js
@@ -1,15 +1,4 @@
-const path = require('path');
-
-const STEROIDS_PATH = '../../dev';
 require('@steroidsjs/webpack').config({
     inlineSvg: true,
     port: 9991,
-    webpack: {
-        resolve: {
-            alias: {
-                '@steroidsjs/core': path.resolve(__dirname, '../react/src'),
-                '@steroidsjs/bootstrap': path.resolve(__dirname, '../react-bootstrap/src'),
-            },
-        },
-    },
 });
