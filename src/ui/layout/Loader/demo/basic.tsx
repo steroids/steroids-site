f27eb179dbe89f568061daba7fdbab11cb63c79c
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-26 13:22:22 +0700

    Add demo for Loader

---
+++ src/ui/layout/Loader/demo/basic.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Loader} from '@steroidsjs/core/ui/layout';
+
+/**
+ * @order 1
+ * @col 6
+ */
+
+export default () => (
+    <>
+        <Loader />
+    </>
+);
