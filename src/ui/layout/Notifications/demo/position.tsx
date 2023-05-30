c6a4fc3feca9f6ef31fd3f6ffd3997681a7c39de
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-05-30 16:41:43 +0800

    Add demos

---
+++ src/ui/layout/Notifications/demo/position.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import useDispatch from '@steroidsjs/core/hooks/useDispatch';
+import {showNotification} from '@steroidsjs/core/actions/notifications';
+import Button from '@steroidsjs/core/ui/form/Button';
+
+/**
+ * Все позиции уведомлений
+ * @order 3
+ * @col 12
+ */
+
+const positions = {
+    tr: 'top-right',
+    tl: 'top-left',
+    br: 'bottom-right',
+    bl: 'bottom-left',
+};
+
+export default () => {
+    const dispatch = useDispatch();
+    return (
+        <div style={{display: 'grid', gridGap: '10px', justifyContent: 'center'}}>
+            {Object.values(positions).map((position: string) => (
+                <Button
+                    key={position}
+                    color='basic'
+                    label={`Позиция ${position}`}
+                    onClick={() => {
+                        dispatch(showNotification(position, 'success', {position, timeOut: 1000}));
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
