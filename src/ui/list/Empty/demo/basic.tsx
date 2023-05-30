abbecd58a8e8c4e80aa86cc07108cd4fcf27acbb
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-05-30 11:58:03 +0800

    Add empty demos

---
+++ src/ui/list/Empty/demo/basic.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Empty from '@steroidsjs/core/ui/list/Empty';
+
+/**
+ * Basic
+ * @order 1
+ * @col 6
+ */
+export default () => (
+    <>
+        <Empty />
+    </>
+);
---
+++ src/ui/list/Empty/demo/custom-text.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import Empty from '@steroidsjs/core/ui/list/Empty';
+
+/**
+ * With custom text
+ * @order 2
+ * @col 6
+ */
+export default () => (
+    <>
+        <Empty
+            text='К сожалению, ничего не найдено'
+        />
+    </>
+);
