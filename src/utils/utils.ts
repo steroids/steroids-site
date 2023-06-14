df6db40b5e3fc2534d9189f2c476a9d9acf42653
Dmitriy <kraxtv471365@gmail.com>
2023-06-14 12:43:42 +0700

    Some changes with PropsTable transition

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {gsap} from 'gsap';
 
+const propsTableSelector = '.ApiTable__title';
+
 export const scrollToPropsTable = () => {
-    gsap.to(window, {duration: 1, scrollTo: document.querySelector('.ApiTable__title')});
+    gsap.to(window, {duration: 1, scrollTo: document.querySelector(propsTableSelector)});
 };
