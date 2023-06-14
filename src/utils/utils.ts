9645b72c152d731c55356907ab777fcf9e13905f
Dmitriy <kraxtv471365@gmail.com>
2023-06-14 15:27:10 +0700

    Review code changes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {gsap} from 'gsap';
 
-const propsTableSelector = '.ApiTable__title';
-
-export const scrollToPropsTable = () => {
-    gsap.to(window, {duration: 1, scrollTo: document.querySelector(propsTableSelector)});
+export const scrollToElement = (elementQuery: string) => {
+    gsap.to(window, {duration: 1, scrollTo: document.querySelector(elementQuery)});
 };
