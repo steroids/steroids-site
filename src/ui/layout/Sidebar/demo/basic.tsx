3aea06a82e21f510684fec1f0a5a19608cddbd78
Dmitriy <kraxtv471365@gmail.com>
2023-06-30 15:36:38 +0700

    Extended Sidebar

--- src/ui/layout/Sidebar/demo/basic.tsx
+++ src/ui/layout/Sidebar/demo/basic.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import Sidebar from '@steroidsjs/core/ui/layout/Sidebar/Sidebar';
 
+const voidFunction = () => {
+
+};
+
 /**
  * Базовое представление Sidebar
  * @order 1
@@ -14,9 +18,37 @@ export default () => (
                 name: 'Konstantin Konstantinonipolsky',
             }}
             logo={{
-                icon: 'danger',
+                icon: 'kozhinDev',
                 label: 'KozhinDev',
             }}
+            menu={{
+                dropDownProps: {
+                    position: 'bottom',
+                    closeMode: 'click-any',
+                },
+                items: [
+                    {label: 'Профиль', icon: 'user', onClick: voidFunction},
+                    {label: 'Настройки', icon: 'setting_line', onClick: voidFunction},
+                    {label: 'Выйти', icon: 'menu_left', onClick: voidFunction},
+                ],
+            }}
+            footerIcons={[
+                {
+                    name: 'vk',
+                },
+                {
+                    name: 'telegram',
+                },
+                {
+                    name: 'whatsapp',
+                },
+                {
+                    name: 'facebook',
+                },
+                {
+                    name: 'instagram',
+                },
+            ]}
         />
     </>
 );
