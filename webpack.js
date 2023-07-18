aa36f9d473127e7736e5745b3310d6bf68624c64
Dmitriy <kraxtv471365@gmail.com>
2023-07-18 12:51:48 +0700

    Fixed a bug with themes mismatch in different instance of ThemesButtons components

--- webpack.js
+++ webpack.js
@@ -1,4 +1,17 @@
+const path = require('path');
+
+const STEROIDS_PATH_CORE = '../react/src';
+const STEROIDS_PATH_BOOTSTRAP = '../react-bootstrap/src';
+
 require('@steroidsjs/webpack').config({
     port: 9991,
     inlineSvg: true,
+    webpack: {
+        resolve: {
+            alias: {
+                '@steroidsjs/core': path.resolve(__dirname, STEROIDS_PATH_CORE),
+                '@steroidsjs/bootstrap': path.resolve(__dirname, STEROIDS_PATH_BOOTSTRAP),
+            },
+        },
+    },
 });
