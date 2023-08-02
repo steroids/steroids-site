bc5c68831e6a74a76ec05c92d6a86ff197226c8f
Dmitriy <kraxtv471365@gmail.com>
2023-08-02 12:09:17 +0700

    Added sidebar demo, updated versions

--- src/ui/layout/Sidebar/demo/basic.tsx
+++ src/ui/layout/Sidebar/demo/basic.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Sidebar from '@steroidsjs/core/ui/layout/Sidebar/Sidebar';
+import {Sidebar} from '@steroidsjs/core/ui/layout';
 
 const voidFunction = () => { };
 
@@ -8,52 +8,116 @@ const voidFunction = () => { };
  * @order 1
  * @col 8
  */
-export default () => (
-    <>
-        <Sidebar
-            user={{
-                picture: '"https://i.ibb.co/bX7Jyqw/Avatars.jpg"',
-                name: 'Konstantin Konstantinonipolsky',
-            }}
-            logo={{
-                icon: 'kozhinDev',
-                label: 'KozhinDev',
-            }}
-            menu={{
-                dropDownProps: {
-                    position: 'bottom',
-                    closeMode: 'click-any',
-                },
-                items: [
-                    {label: 'Профиль', icon: 'user', onClick: voidFunction},
-                    {label: 'Настройки', icon: 'setting_line', onClick: voidFunction},
-                    {label: 'Выйти', icon: 'menu_left', onClick: voidFunction},
-                ],
-            }}
-            footerIcons={[
+export default () => {
+    const props = {
+        user: {
+            picture: '/images/demo/avatar.webp',
+            name: 'Konstantin Konstantinonipolsky',
+        },
+        logo: {
+            icon: 'kozhinDev',
+            label: 'KozhinDev',
+        },
+        menu: {
+            dropDownProps: {
+                position: 'bottom',
+                closeMode: 'click-any',
+            },
+            items: [
+                {label: 'Профиль', icon: 'user', onClick: voidFunction},
+                {label: 'Настройки', icon: 'setting_line', onClick: voidFunction},
+                {label: 'Выйти', icon: 'menu_left', onClick: voidFunction},
+            ],
+        },
+        footerIcons: [
+            {
+                name: 'vk',
+            },
+            {
+                name: 'telegram',
+            },
+            {
+                name: 'whatsapp',
+            },
+            {
+                name: 'facebook',
+            },
+            {
+                name: 'instagram',
+            },
+        ],
+        hasSeparatedNavItem: true,
+        items:
+            [
                 {
-                    name: 'vk',
-                },
-                {
-                    name: 'telegram',
-                },
-                {
-                    name: 'whatsapp',
-                },
-                {
-                    name: 'facebook',
-                },
-                {
-                    name: 'instagram',
-                },
-            ]}
-            items={[
-                {
-                    id: 1,
+                    id: '1',
                     label: 'Dashboard',
-
+                    icon: 'img_box',
+                    badge: {
+                        value: 12,
+                        color: 'primary',
+                    },
                 },
-            ]}
-        />
-    </>
-);
+                {
+                    id: 2,
+                    label: 'Statistics',
+                    icon: 'chart',
+                },
+                {
+                    id: 3,
+                    label: 'Customers',
+                    icon: 'group',
+                },
+                {
+                    id: 4,
+                    label: 'Map',
+                    icon: 'map',
+                },
+                {
+                    id: 5,
+                    label: 'Projects',
+                    icon: 'folder',
+                },
+                {
+                    id: 6,
+                    label: 'Report',
+                    icon: 'pie_chart',
+                },
+                {
+                    id: 7,
+                    label: 'Settings',
+                    icon: 'setting_line',
+                    border: true,
+                    items: [
+                        {
+                            id: 8,
+                            label: 'Point 1',
+                        },
+                        {
+                            id: 9,
+                            label: 'Point 1',
+                        },
+                        {
+                            id: 10,
+                            label: 'Point 1',
+                        },
+                        {
+                            id: 11,
+                            label: 'Point 1',
+                        },
+                    ],
+                },
+                {
+                    id: 12,
+                    label: 'Support',
+                    icon: 'support',
+                },
+            ],
+    };
+
+    return (
+        <>
+            <Sidebar {...props} />
+        </>
+    );
+};
