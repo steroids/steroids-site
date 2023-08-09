cb3b96a2e227f57c705b5e5491ca36702c44f462
Dmitriy <kraxtv471365@gmail.com>
2023-08-09 14:00:39 +0700

    Review code changes

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import useBem from '@steroidsjs/core/hooks/useBem';
-import Link from '@steroidsjs/core/ui/nav/Link';
-import {githubLink} from 'data/indexPageData';
-import {ROUTE_DOCS} from 'constants/routes';
+import Nav from '@steroidsjs/core/ui/nav/Nav';
+import {HEADER_LINKS} from 'constants/links';
 import ThemesButtons from '../ThemesButtons';
 import HeaderLogo from './views/HeaderLogo';
 import SearchInput from './views/SearchInput';
@@ -18,29 +17,11 @@ export default function Header() {
     return (
         <header className={bem.block()}>
             <HeaderLogo />
-            <nav className={bem.element('nav')}>
-                <ul className={bem.element('nav-list')}>
-                    <li className={bem.element('nav-list-item')}>
-                        <Link
-                            toRoute={ROUTE_DOCS}
-                            label={__('Документация')}
-                        />
-                    </li>
-                    <li className={bem.element('nav-list-item')}>
-                        <Link
-                            toRoute={ROUTE_DOCS}
-                            label={__('Набор статей')}
-                        />
-                    </li>
-                    <li className={bem.element('nav-list-item')}>
-                        <Link
-                            url={githubLink}
-                            target='blank'
-                            label={__('GIT')}
-                        />
-                    </li>
-                </ul>
-            </nav>
+            <Nav
+                items={HEADER_LINKS}
+                layout="link"
+                className={bem.element('nav')}
+            />
             <div className={bem.element('controls')}>
                 <SearchInput />
                 <ThemesButtons className={bem.element('controls-themes')} />
