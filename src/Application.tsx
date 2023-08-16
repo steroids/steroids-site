a76b0e7f825e598dcc0ecc84e88abea5719f4f87
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-08-16 13:05:48 +0800

    Begin

--- src/Application.tsx
+++ src/Application.tsx
@@ -5,7 +5,7 @@ import HttpComponent from '@steroidsjs/core/components/HttpComponent';
 import LocaleComponent from '@steroidsjs/core/components/LocaleComponent';
 import ResourceComponent from '@steroidsjs/core/components/ResourceComponent';
 import {gsap} from 'gsap';
-import {ScrollToPlugin} from 'gsap/ScrollToPlugin';
+import {ScrollToPlugin} from 'gsap/dist/ScrollToPlugin';
 import {icons} from 'icons';
 import {detectLanguage} from 'hooks/useAppLanguage';
 import {translationsMap} from 'constants/translationsMap';
@@ -13,34 +13,36 @@ import {translationsMap} from 'constants/translationsMap';
 import 'style/index.scss';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
-export default function Application() {
-    const {renderApplication} = useApplication({
-        reducers: require('@steroidsjs/core/reducers').default,
-        routes: () => require('routes').default,
-        layoutView: () => require('shared/Layout').default,
-        screen: {},
-        theme: {},
-        components: {
-            locale: LocaleComponent,
-            http: HttpComponent,
-            resource: ResourceComponent,
-        },
-        onInit: ({ui, locale, store}) => {
-            // В момент создания в LocaleComponent язык указывается только на основе config'а, переданного в компонент
-            // Но на этом этапе нет возможности передать правильный язык (или функцию определения правильного языка)
-            // в компонент, поэтому мы устанавливаем язык после того, как компонент был проинициализирован
-            // (функция onInit) вызывается после инициализации всех компонентов
-            // @todo нужно перенести определение и установку языка в кастомный LocaleComponent, проверив и работу SSR
-            const contextLanguage = detectLanguage(store.history.location.pathname);
-            locale.language = contextLanguage;
-            locale.translations = translationsMap[contextLanguage];
+export const config = {
+    reducers: require('@steroidsjs/core/reducers').default,
+    routes: () => require('routes').default,
+    layoutView: () => require('shared/Layout').default,
+    screen: {},
+    theme: {},
+    components: {
+        locale: LocaleComponent,
+        http: HttpComponent,
+        resource: ResourceComponent,
+    },
+    onInit: ({ui, locale, store}) => {
+        // В момент создания в LocaleComponent язык указывается только на основе config'а, переданного в компонент
+        // Но на этом этапе нет возможности передать правильный язык (или функцию определения правильного языка)
+        // в компонент, поэтому мы устанавливаем язык после того, как компонент был проинициализирован
+        // (функция onInit) вызывается после инициализации всех компонентов
+        // @todo нужно перенести определение и установку языка в кастомный LocaleComponent, проверив и работу SSR
+        const contextLanguage = detectLanguage(store.history.location.pathname);
+        locale.language = contextLanguage;
+        locale.translations = translationsMap[contextLanguage];
 
-            ui.addViews(require('./ui/bootstrap').default);
-            ui.addFields(require('@steroidsjs/core/ui/form').default);
-            ui.addFormatters(require('@steroidsjs/core/ui/format').default);
-            ui.addIcons(require('@steroidsjs/bootstrap/icons/index').default(icons));
-        },
-    });
+        ui.addViews(require('./ui/bootstrap').default);
+        ui.addFields(require('@steroidsjs/core/ui/form').default);
+        ui.addFormatters(require('@steroidsjs/core/ui/format').default);
+        ui.addIcons(require('@steroidsjs/bootstrap/icons/index').default(icons));
+    },
+};
+
+export default function Application() {
+    const {renderApplication} = useApplication(config);
 
     React.useEffect(() => {
         gsap.registerPlugin(ScrollToPlugin);
--- webpack.js
+++ webpack.js
@@ -1,4 +1,20 @@
+const webpackNodeExternals = require('webpack-node-externals');
+
 require('@steroidsjs/webpack').config({
-    port: 9991,
+    port: process.env.APP_SSR_PORT || 9991,
     inlineSvg: true,
+    ssr: {
+        externals: [
+            webpackNodeExternals({
+                allowlist: [/\.(?!(?:tsx?|jsx?|json)$).{1,5}$/i, /^lodash/, /^@steroidsjs/],
+            }),
+        ],
+        module: {
+            rules: {
+                ts: {
+                    exclude: /\.\/node_modules\/(?!steroidsjs\/core\/).*/,
+                },
+            },
+        },
+    },
 });
